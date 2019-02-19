a130e78ae501c8b4ccf4255ae4745d5c6ba492a2
Daniel Sobczak <daniel.sobczak@tigerspike.com>
2019-02-19 21:34:31 +0100

    added navigation to post details

--- app/src/main/java/com/sample/test_posts_app/presentation/feed/HomeFeedFragment.kt
+++ app/src/main/java/com/sample/test_posts_app/presentation/feed/HomeFeedFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sample.test_posts_app.R
@@ -16,13 +17,12 @@ import kotlinx.android.synthetic.main.feed_item.view.*
 import kotlinx.android.synthetic.main.fragment_home_feed.*
 
 class HomeFeedFragment : Fragment() {
-    private val feedAdapter = FeedAdapter()
+    private val feedAdapter = FeedAdapter(this::onPostClicked)
     private lateinit var viewModel: FeedViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home_feed, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -67,13 +67,22 @@ class HomeFeedFragment : Fragment() {
         feedList.visibility = View.VISIBLE
         feedAdapter.setPosts(posts)
     }
+
+    private fun onPostClicked(post: Post) {
+        val navAction = HomeFeedFragmentDirections.actionHomeFeedFragmentToPostDetailFragment(post.id)
+        findNavController().navigate(navAction)
+    }
 }
 
-class FeedAdapter : RecyclerView.Adapter<FeedVH>() {
+typealias PostClickListener = (Post) -> Unit
+
+class FeedAdapter(private val clickListener: PostClickListener) : RecyclerView.Adapter<FeedVH>() {
     private var posts: List<Post> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedVH {
-        return FeedVH.create(parent)
+        val feedVH = FeedVH.create(parent)
+        feedVH.itemView.setOnClickListener { clickListener.invoke(posts[feedVH.adapterPosition]) }
+        return feedVH
     }
 
     override fun getItemCount(): Int {
