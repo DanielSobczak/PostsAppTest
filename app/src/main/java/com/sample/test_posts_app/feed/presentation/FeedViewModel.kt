3dabd499a31c8655704fc113232c53b08b6dab0f
Daniel Sobczak <daniel.sobczak@tigerspike.com>
2019-02-19 18:13:03 +0100

    added FeedViewModel with tests

---
+++ app/src/main/java/com/sample/test_posts_app/feed/presentation/FeedViewModel.kt
@@ -0,0 +1,70 @@
+package com.sample.test_posts_app.feed.presentation
+
+import com.sample.test_posts_app.feed.domain.GetPostsUseCase
+import com.sample.test_posts_app.feed.domain.Post
+import com.ww.roxie.BaseAction
+import com.ww.roxie.BaseState
+import com.ww.roxie.BaseViewModel
+import io.reactivex.Scheduler
+import io.reactivex.rxkotlin.ofType
+import io.reactivex.rxkotlin.plusAssign
+
+sealed class Action : BaseAction {
+    object LoadFeed : Action()
+}
+
+data class State(
+    val posts: List<Post> = emptyList(),
+    val isIdle: Boolean = false,
+    val isLoading: Boolean = false,
+    val isError: Boolean = false
+) : BaseState {
+    fun forLoading() = copy(isIdle = false, isLoading = true, isError = false, posts = emptyList())
+    fun forReceivedData(posts: List<Post>) = copy(isLoading = false, posts = posts)
+    fun forError() = copy(isLoading = false, isError = true)
+}
+
+sealed class Change {
+    object Loading : Change()
+    data class Posts(val posts: List<Post>) : Change()
+    data class Error(val throwable: Throwable?) : Change()
+}
+
+class FeedViewModel(
+    initialState: State?,
+    private val getPostsUseCase: GetPostsUseCase,
+    private val backgroundScheduler: Scheduler,
+    private val foregroundScheduler: Scheduler
+) : BaseViewModel<Action, State>() {
+
+    override val initialState = initialState ?: State(isIdle = true)
+
+    init {
+        bindData()
+    }
+
+    private fun bindData() {
+        disposables += actions.ofType<Action.LoadFeed>()
+            .switchMap {
+                getPostsUseCase.getAllPosts()
+                    .subscribeOn(backgroundScheduler)
+                    .toObservable()
+                    .map<Change> { Change.Posts(it) }
+                    .defaultIfEmpty(Change.Posts(emptyList()))
+                    .startWith(Change.Loading)
+                    .onErrorReturn { Change.Error(it) }
+            }
+            .scan(initialState) { state, change ->
+                when (change) {
+                    is Change.Loading -> state.forLoading()
+                    is Change.Posts -> state.forReceivedData(change.posts)
+                    is Change.Error -> state.forError()
+                }
+            }
+            .filter { !it.isIdle }
+            .distinctUntilChanged()
+            .observeOn(foregroundScheduler)
+            .subscribe { state.value = it }
+    }
+
+}
---
+++ app/src/test/java/com/sample/test_posts_app/feed/presentation/FeedViewModelTest.kt
@@ -0,0 +1,71 @@
+package com.sample.test_posts_app.feed.presentation
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.*
+import com.sample.test_posts_app.feed.domain.GetPostsUseCase
+import com.sample.test_posts_app.feed.domain.Post
+import io.reactivex.Single
+import io.reactivex.schedulers.TestScheduler
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class FeedViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+    private val testScheduler = TestScheduler()
+
+    private val idleState = State(isIdle = true)
+    private val loadingState = State(isLoading = true)
+    private val failedState = State(isError = true)
+    private val posts = listOf(Post(1, "foo", "bar", 1234))
+    private val mockGetPostsUseCase = mock<GetPostsUseCase>()
+    private val mockObserver = mock<Observer<State>>()
+
+    private lateinit var sut: FeedViewModel
+
+    @Before
+    fun setUp() {
+        sut = FeedViewModel(idleState, mockGetPostsUseCase, testScheduler, testScheduler)
+    }
+
+    @Test
+    fun `When no action is send, no action is emitted`() {
+        given(mockGetPostsUseCase.getAllPosts()).willReturn(Single.just(posts))
+
+        sut.observableState.observeForever(mockObserver)
+        testScheduler.triggerActions()
+
+        verify(mockObserver, never()).onChanged(any())
+    }
+
+    @Test
+    fun `Given posts loaded successfully, when action received, emitting state with posts`() {
+        val successState = State(posts)
+        given(mockGetPostsUseCase.getAllPosts()).willReturn(Single.just(posts))
+
+        sut.observableState.observeForever(mockObserver)
+        sut.dispatch(Action.LoadFeed)
+        testScheduler.triggerActions()
+
+        inOrder(mockObserver) {
+            verify(mockObserver).onChanged(loadingState)
+            verify(mockObserver).onChanged(successState)
+        }
+    }
+
+    @Test
+    fun `Given posts loading failed, when action received, emitting error state`() {
+        given(mockGetPostsUseCase.getAllPosts()).willReturn(Single.error(RuntimeException()))
+
+        sut.observableState.observeForever(mockObserver)
+        sut.dispatch(Action.LoadFeed)
+        testScheduler.triggerActions()
+
+        inOrder(mockObserver) {
+            verify(mockObserver).onChanged(loadingState)
+            verify(mockObserver).onChanged(failedState)
+        }
+    }
+}
